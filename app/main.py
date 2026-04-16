8094ec6649b9e769b6379cb86b1e4a892811d1b0
Ethan <eborror@oakland.edu>
2026-04-16 11:12:44 -0400

    modified:   app/main.py 	modified:   templates/dashboard.html

--- app/main.py
+++ app/main.py
@@ -26,37 +26,51 @@ def get_db_conn():
     return db_pool.get_connection()
 
 @app.get("/")
-def dashboard(request: Request):
+def home(request: Request):
     conn = get_db_conn()
     cursor = conn.cursor(dictionary=True)
 
     try:
-        cursor.execute("SELECT * FROM Hubs")
-        hubs = cursor.fetchall()
+        cursor.execute("SELECT * FROM PRODUCT")
+        products = cursor.fetchall()
 
-        return templates.TemplateResponse(
-            "dashboard.html",
-            {
-                "request": request,
-                "hubs": hubs
-            }
-        )
+        return {"products": products}
+
+    except Exception as e:
+        return {"error": str(e)}
+
     finally:
         cursor.close()
         conn.close()
 
-@app.post("/checkout")
-def checkout(member_id: int = Form(...), serial_num: str = Form(...)):
+@app.post("/customer")
+def add_customer(
+    customer_id: int = Form(...),
+    first_name: str = Form(...),
+    last_name: str = Form(None),
+    email: str = Form(None),
+    phone: str = Form(None)
+):
     conn = get_db_conn()
     cursor = conn.cursor()
+
     try:
-        cursor.execute("INSERT INTO Rentals (MemberID, SerialNum, StartTime) VALUES (%s, %s, NOW())", (member_id, serial_num))
-        cursor.execute("UPDATE Bikes SET Status = 'In-Use', HubID = NULL WHERE SerialNum = %s", (serial_num,))
+        cursor.execute("""
+            INSERT INTO CUSTOMER 
+            (CUSTOMER_ID, CUSTOMER_FNAME, CUSTOMER_LNAME, CUSTOMER_EMAIL, CUSTOMER_PHONE)
+            VALUES (%s, %s, %s, %s, %s)
+        """, (customer_id, first_name, last_name, email, phone))
+
         conn.commit()
     except Exception as e:
         conn.rollback()
-        print(f"Error: {e}")
+        print(e)
     finally:
         cursor.close()
         conn.close()
+
     return RedirectResponse(url="/", status_code=303)
+
+@app.get("/test")
+def test():
+    return {"status": "working"}
